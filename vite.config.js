60ac0ecc22c5d150cacc6ec9443a542f91edac95
Bun He <hungoffsize@gmail.com>
2024-09-25 23:23:16 +0700

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 10000, // Chỉ định cổng cụ thể
+    host: '0.0.0.0', // Cho phép truy cập từ bên ngoài
+  },
 })
